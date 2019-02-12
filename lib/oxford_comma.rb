c4f40299f03fedd46f067bb985f4570aa4b9ea3d
Alicia Santiago <albrown07@mac.com>
2019-02-12 15:09:02 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,12 +3,9 @@ def oxford_comma(array)
     array.join
   elsif array.length==2 
     array.join(" and ")
-    
+  elsif array.length==3
+    array.join
 
 def oxford_comma(array)
   array.join(" and ")
-end
-
-def oxford_comma(array)
-  array.join
 end
